da227ce3382defcb94255ed2a42c189767ff71d4
Pratik Khanolkar <pratikid@gmail.com>
2015-08-31 23:03:12 +0530

    Input multiple Subject_code
    This allows user to download question paper of more than one subject in
    a go.

--- Gquipe/Form1.Designer.cs
+++ Gquipe/Form1.Designer.cs
@@ -31,13 +31,14 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Location = new System.Drawing.Point(117, 12);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(760, 20);
+            this.textBox1.Size = new System.Drawing.Size(655, 20);
             this.textBox1.TabIndex = 0;
             // 
             // folderBrowserDialog1
@@ -46,7 +47,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(329, 427);
+            this.button1.Location = new System.Drawing.Point(117, 91);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(159, 23);
             this.button1.TabIndex = 1;
@@ -54,11 +55,21 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter Subject Code";
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.ClientSize = new System.Drawing.Size(784, 126);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.MaximizeBox = false;
@@ -77,6 +88,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Gquipe/Form1.cs
+++ Gquipe/Form1.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
+using System.IO;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text;
 
 namespace Gquipe
 {
@@ -25,22 +21,103 @@ namespace Gquipe
 
         private void button1_Click(object sender, EventArgs e)
         {
-            DialogResult result = folderBrowserDialog1.ShowDialog();
 
-            if(result==DialogResult.OK)
+            if (textBox1.Text == "")
             {
+                textBox1.Focus();
+            }
+            else if(textBox1.Text!="")
+            {
+                string input_sub_code;
+                string[] sub_code = textBox1.Text.Split(',');
+                for (int i = 0; i < sub_code.Length; i++)
+                {
+                    sub_code[i] = sub_code[i] + ".zip";
+                }
+                input_sub_code=ConvertStringArrayToString(sub_code);
+                MessageBox.Show(input_sub_code);
+            
+                DialogResult result = folderBrowserDialog1.ShowDialog();
                 string foldername = this.folderBrowserDialog1.SelectedPath;
-                System.Diagnostics.Process process = new System.Diagnostics.Process();
-                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
-                //startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
-                startInfo.FileName = "cmd.exe";
-                startInfo.Arguments = "/C wget --continue --timestamping --directory-prefix="+foldername+" --recursive --level=0 --accept=170701.zip  http://www.gtu.ac.in/Qpaper.html > error.log";
-                process.StartInfo = startInfo;
-                
-                process.Start();
-                process.WaitForExit();
-                Console.Write(process.StandardOutput.ReadToEnd());
+                string output = string.Empty;
+                string error = string.Empty;
+
+                if (result == DialogResult.OK)
+                {
+                    ProcessStartInfo processStartInfo = new ProcessStartInfo("cmd", "/C wget --continue --timestamping --directory-prefix=" + foldername + " --recursive --level=1 --accept="+input_sub_code +" http://www.gtu.ac.in/Qpaper.html");
+                    processStartInfo.RedirectStandardOutput = true;
+                    processStartInfo.RedirectStandardError = true;
+                    processStartInfo.WindowStyle = ProcessWindowStyle.Normal;
+                    processStartInfo.UseShellExecute = true;
+
+                    Process process = Process.Start(processStartInfo);
+
+
+                    using (StreamReader streamReader = process.StandardOutput)
+                    {
+                        output = streamReader.ReadToEnd();
+                    }
+
+                    using (StreamReader streamReader = process.StandardError)
+                    {
+                        error = streamReader.ReadToEnd();
+                    }
+
+                    //Console.WriteLine("The following output was detected:");
+                    //Console.WriteLine(output);
+
+                    if (!string.IsNullOrEmpty(error))
+                    {
+                        //Console.WriteLine("The following error was detected:");
+                        Console.WriteLine(error);
+                    }
+
+                    //Console.Read();
+                }
+
+                MessageBox.Show(Application.StartupPath);
+
+                ProcessStartInfo opencurrentdir = new ProcessStartInfo("cmd", "/C " + foldername.Substring(0, 2).ToString());
+                opencurrentdir.RedirectStandardOutput = true;
+                opencurrentdir.RedirectStandardError = true;
+                //processStartInfo1.WindowStyle = ProcessWindowStyle.Maximized;
+                opencurrentdir.WindowStyle = ProcessWindowStyle.Normal;
+                opencurrentdir.UseShellExecute = false;
+
+                Process process1 = new Process();
+                process1.StartInfo = opencurrentdir;
+                //process1.WaitForExit(10000);
+                process1.Start();
+
+
+                ProcessStartInfo setpath = new ProcessStartInfo("cmd", "/C " + foldername.Substring(0, 2).ToString());
+                setpath.RedirectStandardOutput = true;
+                setpath.RedirectStandardError = true;
+                setpath.WindowStyle = ProcessWindowStyle.Normal;
+                setpath.UseShellExecute = false;
+
+                process1 = Process.Start(setpath);
+                process1.WaitForExit();
+
+                //E:\Github\Gquipe\Gquipe\bin\Debug
+
+
             }
         }
+
+        static string ConvertStringArrayToString(string[] array)
+        {
+            //
+            // Concatenate all the elements into a StringBuilder.
+            //
+            StringBuilder builder = new StringBuilder();
+            foreach (string value in array)
+            {
+                builder.Append(value);
+                builder.Append('.');
+            }
+            return builder.ToString();
+        }
+
     }
 }
