71a29c3e930d4a6663d68efd7bc48b8324c3ae82
Pratik Khanolkar <pratikid@gmail.com>
2015-09-02 01:13:01 +0530

    Empty Folders Removed

--- Gquipe/Form1.cs
+++ Gquipe/Form1.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
 using System.Text;
+using System.Linq;
 
 namespace Gquipe
 {
@@ -16,12 +17,11 @@ namespace Gquipe
 
         private void Main_Load(object sender, EventArgs e)
         {
-            folderBrowserDialog1.RootFolder = Environment.SpecialFolder.MyComputer;
+            //folderBrowserDialog1.RootFolder = Environment.SpecialFolder.MyComputer;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
             if (textBox1.Text == "")
             {
                 textBox1.Focus();
@@ -35,24 +35,27 @@ namespace Gquipe
                     sub_code[i] = sub_code[i] + ".zip";
                 }
                 input_sub_code=ConvertStringArrayToString(sub_code);
-                MessageBox.Show(input_sub_code);
-            
-                DialogResult result = folderBrowserDialog1.ShowDialog();
-                string foldername = this.folderBrowserDialog1.SelectedPath;
+                input_sub_code=input_sub_code.Substring(0, input_sub_code.Length - 1);
+
+                string foldername=null;
                 string output = string.Empty;
                 string error = string.Empty;
 
-                if (result == DialogResult.OK)
+                if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
                 {
-                    ProcessStartInfo processStartInfo = new ProcessStartInfo("cmd", "/C wget --continue --timestamping --directory-prefix=" + foldername + " --recursive --level=0 --accept="+input_sub_code +" http://www.gtu.ac.in/Qpaper.html");
-                    processStartInfo.RedirectStandardOutput = true;
-                    processStartInfo.RedirectStandardError = true;
-                    processStartInfo.WindowStyle = ProcessWindowStyle.Normal;
-                    processStartInfo.UseShellExecute = false;
+                    foldername = this.folderBrowserDialog1.SelectedPath;
+                    ProcessStartInfo processStartInfo = new ProcessStartInfo("cmd", "/C wget --continue --timestamping --directory-prefix=" + foldername + " --recursive --level=0 --accept="+input_sub_code+" http://www.gtu.ac.in/Qpaper.html");
+                    //processStartInfo.RedirectStandardOutput = true;
+                    //processStartInfo.RedirectStandardError = true;
+                    processStartInfo.WindowStyle = ProcessWindowStyle.Maximized;
+                    //processStartInfo.UseShellExecute = false;
 
                     Process process = Process.Start(processStartInfo);
+                    process.WaitForExit();
+                    
+ 
 
-
+                    /*
                     using (StreamReader streamReader = process.StandardOutput)
                     {
                         output = streamReader.ReadToEnd();
@@ -73,9 +76,11 @@ namespace Gquipe
                     }
                     process.WaitForExit(6000000);
                     //Console.Read();
+                    */
                 }
 
-                MessageBox.Show(Application.StartupPath);
+                //MessageBox.Show(Application.StartupPath);
+                
                 /*
                 ProcessStartInfo opencurrentdir = new ProcessStartInfo("cmd", "/C " + foldername.Substring(0, 2).ToString());
                 opencurrentdir.RedirectStandardOutput = true;
@@ -88,36 +93,45 @@ namespace Gquipe
                 process1.StartInfo = opencurrentdir;
                 //process1.WaitForExit(10000);
                 process1.Start();
-
-
+                 * 
                 ProcessStartInfo setpath = new ProcessStartInfo("cmd", "/C " + foldername.Substring(0, 2).ToString());
                 setpath.RedirectStandardOutput = true;
                 setpath.RedirectStandardError = true;
                 setpath.WindowStyle = ProcessWindowStyle.Normal;
                 setpath.UseShellExecute = false;
-
                 process1 = Process.Start(setpath);
                 process1.WaitForExit();
 
-                //E:\Github\Gquipe\Gquipe\bin\Debug
                 */
 
+                RemoveEmptyFolders(foldername + @"\www.gtu.ac.in\GTU_Papers");
+                MessageBox.Show("Complete");
             }
+        }
+
+        private void RemoveEmptyFolders(string path)
+        {
+            foreach (string subFolder in Directory.GetDirectories(path))
+                RemoveEmptySubFolders(subFolder);
+        }
+
+        private bool RemoveEmptySubFolders(string path)
+        {
+            bool isEmpty = Directory.GetDirectories(path).Aggregate(true, (current, subFolder) => current & RemoveEmptySubFolders(subFolder))
+                && Directory.GetFiles(path).Length == 0;
+            if (isEmpty)
+                Directory.Delete(path);
+            return isEmpty;
         }
 
         static string ConvertStringArrayToString(string[] array)
         {
-            //
-            // Concatenate all the elements into a StringBuilder.
-            //
             StringBuilder builder = new StringBuilder();
             foreach (string value in array)
             {
-                builder.Append(value);
-                builder.Append(',');
+                builder.Append(value+',');
             }
             return builder.ToString();
         }
-
     }
 }
